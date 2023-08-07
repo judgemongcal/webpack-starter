2d0917aeefc651b6b3c5a86e5e1f23445a348ef5
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-08-07 20:43:26 +0800

    210 - Webpack Basic Setup

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+// Uses CommonJS
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+};
